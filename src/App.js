e0353a8a585b12584b462543836c107893958184
Reyvan clief <rey17van17@gmail.com>
2024-11-12 00:21:46 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Clients from "./components/Client";
+import Feature from "./components/Feature";
+import Pricing from "./components/Pricing";
+import Footer from "./components/Footer";
+
+
+
+function App() {
+  <div className="body-wrap">
+    <Header />
+    <main className="site-content">
+      <Hero />
+      <Clients />
+      <Feature />
+      <Pricing />
+    </main>
+      <Footer />
+  </div>;
+}
+
+export default App;
